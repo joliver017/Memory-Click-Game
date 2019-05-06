5e88c3b872b803fdbb4c24546b357e2350048ae6
Oliver 2wist <joliver017@gmail.com>
2019-05-05 19:31:51 -0500

    Updates

---
+++ precache-manifest.7c2e947d7a3d8417a82522d09b5eef91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211d212c583d8d65a2559fc937626ab7",
+    "url": "/Memory-Click-Game/index.html"
+  },
+  {
+    "revision": "437ccd3f79d1a65f8f67",
+    "url": "/Memory-Click-Game/static/css/main.3798c34f.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Memory-Click-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "437ccd3f79d1a65f8f67",
+    "url": "/Memory-Click-Game/static/js/main.5554c3b4.chunk.js"
+  },
+  {
+    "revision": "86e2140e30f9c97224dd",
+    "url": "/Memory-Click-Game/static/js/runtime~main.cb345ba7.js"
+  }
+]);
